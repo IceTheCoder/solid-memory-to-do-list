06556c648af8ed725d2d8bfc104168bb5f0660dc
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-05-14 18:51:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ function addTask() {
 	inputBox.value = "";
 }
 
-listContainer.addEventListener("click", function(e){}); {
+listContainer.addEventListener("click", function(e) {
 	if (e.target.tagName === "LI") {
 		e.target.classList.toggle("checked");
 	}
 	else if (e.target.tagName === "SPAN") {
 		e.target.parentElement.remove();
 	}
-}
+}, false)
