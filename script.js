d55f174e4a4cee143d6daf088291119471b45943
IceTheDev2 <cristian.gabriel131408@gmail.com>
2023-05-14 18:43:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function addTask() {
     let li = document.createElement("li");
 		li.innerHTML = inputBox.value;
 		listContainer.appendChild(li);
+		let span = document.createElement("span");
+		span.innerHTML = "\u00d7";	
   }
 	inputBox.value = "";
 }
